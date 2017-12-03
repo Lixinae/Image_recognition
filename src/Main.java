d6acb159688692cd1da5b9685d2f9588d60bcbb8
kevblond <kevinblondel@msn.com>
2017-12-03 19:58:10 +0100

    recuperation image

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
 
-    private static int recup_image(){
+    private static BufferedImage recup_image() throws IOException {
         //recup image dans image_cible
-        return 0;
+        File f = new File("K:\\users\\kev\\documents\\Image_recognition\\image_cible\\cible1.jpg");
+        return ImageIO.read(f);
+
     }
     private static ArrayList<Integer> recup_image_database(){
         //recup image dans image_base
         return new ArrayList<>();
     }
     public static void main(String[] args){
+
         System.out.println("deb");
-        int image = recup_image();
-        Image im = new Image(image);
+        BufferedImage image = new BufferedImage(833, 1200, BufferedImage.TYPE_INT_ARGB);
+        File f = null;
+        try {
+            image = recup_image();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //https://docs.oracle.com/javase/tutorial/2d/images/index.html
+        //https://www.programcreek.com/java-api-examples/index.php?class=java.awt.image.BufferedImage&method=setRGB
+        //https://docs.oracle.com/javase/7/docs/api/java/awt/image/BufferedImage.html
         ArrayList<Integer> image_id_database = recup_image_database();
         ArrayList<Image> image_database = new ArrayList<>();
         for(int img:image_id_database){
